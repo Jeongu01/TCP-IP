c640c7d2ea861f059a2fb313d1b57dcd79b17b2e
Jeongu01 <kjw101147@gmail.com>
2024-07-11 22:09:23 +0900

    ✨ feat : 상품 관리 프로그램 구현

---
+++ src/Request.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Request{
+  private int menu;
+  private Product data;
+
+}
---
+++ src/Response.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Response{
+  private String status;
+  private ArrayList<Product> data;
+}
